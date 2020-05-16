7cfee61e450e7542f908a97b601b1e9aa83dfc48
bigmeco <bigmecoprog@gmail.com>
2020-05-17 02:41:12 +0300

    I remembered how to make a request at least

--- app/mvitest/src/main/java/com/bigmeco/mvitest/MainActivity.kt
+++ app/mvitest/src/main/java/com/bigmeco/mvitest/MainActivity.kt
@@ -1,16 +1,50 @@
 package com.bigmeco.mvitest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://www.boredapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
+
+//        GlobalScope.launch(Dispatchers.Main) {
+            val service: Data = retrofit.create(Data::class.java)
+            service.search().enqueue(object : Callback<Repo> {
+
+
+                override fun onResponse(call: Call<Repo>, response: Response<Repo>) {
+                    Log.d("Tag", response.body()?.activity)
+
+                }
+
+                override fun onFailure(call: Call<Repo>, t: Throwable) {
+                    Log.d("Tag", t.message)
+
+                }
+            })
+//        }
+
         setContent {
             MaterialTheme {
                 Greeting("Android")
---
+++ app/mvitest/src/main/java/com/bigmeco/mvitest/Repo.kt
@@ -0,0 +1,10 @@
+package com.bigmeco.mvitest
+
+data class Repo(
+    val activity: String,
+    val accessibility: Long,
+    val type: String,
+    val price: Long,
+    val link: String,
+    val key: String
+)
