e77f787685fb95a3f87dd25d8f1e7c1e1b0009a2
bigmeco <bigmecoprog@gmail.com>
2020-07-06 14:45:40 +0300

    update gradle build

--- app/src/main/java/com/bigmeco/testarchitectures/MainActivity.kt
+++ app/src/main/java/com/bigmeco/testarchitectures/MainActivity.kt
@@ -3,10 +3,11 @@ package com.bigmeco.testarchitectures
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
